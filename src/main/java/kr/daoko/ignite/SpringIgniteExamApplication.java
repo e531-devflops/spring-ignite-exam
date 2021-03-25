09a3f40cd4b18b5662be91f7217a9e676b075b57
e531-devflops <e531.devflops@gmail.com>
2021-03-25 14:25:14 +0900

    initial commit

---
+++ src/main/java/kr/daoko/ignite/SpringIgniteExamApplication.java
@@ -0,0 +1,13 @@
+package kr.daoko.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIgniteExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIgniteExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/daoko/ignite/SpringIgniteExamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.daoko.ignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIgniteExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
